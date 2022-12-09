6e134ed72d11f18104297cfd58e98598815f351f
Jefferson França <jefferson.vieiraf@hotmail.com>
2022-12-08 22:39:03 -0300

    refactor: adiciona icones na barra de pesquisa

--- components/searchBar.tsx
+++ components/searchBar.tsx
@@ -1,19 +1,22 @@
-import { GiHamburgerMenu } from 'react-icons/gi';
-import { FaUserCircle } from 'react-icons/fa';
+import { FaRegUserCircle } from 'react-icons/fa';
 import { BsSearch } from 'react-icons/bs';
+import { AiOutlineShoppingCart } from 'react-icons/ai';
 import styles from '../styles/searchBar.module.css';
 
 export default function SearchBar(){
   return(
     <div className={styles.nav_search}>
-      <GiHamburgerMenu className={styles.nav_search_icon} aria-label='Menu lateral'/>
+      <h1 className={styles.title}>Compre+</h1>
       <form className={styles.search_form}role={'search'} action={'/'} method={'get'}>
         <input className={styles.input} type={'text'} aria-label='Buscar'></input>
         <button className={styles.submit_btn} type='submit' aria-label='Confirmar busca'>
             <BsSearch className={styles.search_icon}/>
         </button>
       </form>
-      <FaUserCircle className={styles.nav_search_icon} aria-label='Perfil'/>
+      <div className={styles.icons_div}>
+        <AiOutlineShoppingCart className={styles.nav_search_icon} aria-label='Carrinho'/>
+        <FaRegUserCircle className={styles.nav_search_icon} aria-label='Perfil'/>
+      </div>
     </div>
   )
 }
