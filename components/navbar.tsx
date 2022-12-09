066211dcb0f90ca3126d5c368d27a15a9e6e5b4e
Jefferson França <jefferson.vieiraf@hotmail.com>
2022-12-08 23:52:20 -0300

    refactor: adiciona links para as opcoes da navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,15 +1,26 @@
+import Link from "next/link";
 import styles from "../styles/navbar.module.css";
 
 export default function Navbar(){
   return(
     <nav className={styles.navbar}>
       <ul className={styles.navbar_options}>
-        <li>Home</li>
-        <li>Ofertas do Dia</li>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/ofertas">Ofertas do Dia</Link>
+        </li>
         <li>Populares</li>
-        <li>Celulares</li>
-        <li>Hardware</li>
-        <li>Jogos</li>
+        <li>
+          <Link href="/categoria/celulares">Celulares</Link>
+        </li>
+        <li>
+          <Link href="/categoria/hardware">Hardware</Link>
+        </li>
+        <li>
+          <Link href="/categoria/jogos">Jogos</Link>
+        </li>
       </ul>
     </nav>
   )
